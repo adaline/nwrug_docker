6f85e1881659036fee8164798fa401c3ef8ba242
Val <arkhipov.valentin@gmail.com>
2015-07-15 23:02:00 +0100

    Added some production ENV vars

--- guestbook/start.sh
+++ guestbook/start.sh
@@ -1,1 +1,1 @@
-docker run -d --name nwrug_guestbook -p 3000:3000 nwrug/guestbook
+docker run -d --name nwrug_guestbook -e RAILS_ENV=production -e RAILS_SERVE_STATIC_FILES=true -e SECRET_KEY_BASE=dont_tell -p 3000:3000 nwrug/guestbook
